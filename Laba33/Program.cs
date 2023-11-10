b0400aa4afc0c0c3512a3ef2bc9c1707a843fe66
Arion <Arion@DESKTOP-9BU80RS>
2023-11-10 20:21:38 +0200

    Добавьте файлы проекта.

---
+++ Laba33/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите n: ");
+    int n = int.Parse(Console.ReadLine());
+    double s = 1;
+    for (int k = 1; k <= 1; k += 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            long f = 1;
+            for (int j = 1; j <= 1; j++) f *= j;
+            if (i % 2 == 0)
+            {
+                s += (k / (1 * f) * (Math.Pow((x / 2), i)));
+            }
+            Console.Write($"S={s:F2}");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
